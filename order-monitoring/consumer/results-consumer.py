dd9050716a2d2aff902dc2fdc43dfe766a3a0722
hendrikE <hendrik-eichhorn@web.de>
2020-07-15 09:54:09 +0200

    added dashboard and database

--- order-monitoring/consumer/results-consumer.py
+++ order-monitoring/consumer/results-consumer.py
@@ -6,6 +6,7 @@ import time
 
 from kafka.errors import NoBrokersAvailable
 from kafka import KafkaConsumer
+import pymysql
 
 from messages_pb2 import Report
 
@@ -13,6 +14,17 @@ KAFKA_BROKER = "kafka-broker:9092"
 
 
 def consume():
+    db_connection = pymysql.connect(
+        host="database:3306",
+        user="user",
+        password="password",
+        db="lieferbot"
+    )
+    db_cursor = db_connection.cursor()
+    query = "CREATE TABLE reports(order VARCHAR(255), vehicle VARCHAR(255), unassigned VARCHAR(255), " \
+            "assigned VARCHAR(255), inprogress VARCHAR(255), delivered VARCHAR(255), throughput VARCHAR(255))"
+    db_cursor.execute(query)
+    db_connection.commit()
     consumer = KafkaConsumer(
         'reports',
         bootstrap_servers=[KAFKA_BROKER],
@@ -21,16 +33,23 @@ def consume():
         response = Report()
         response.ParseFromString(message.value)
 
+        str_unassigned = datetime.utcfromtimestamp(response.timeUnassigned).strftime('%Y-%m-%d %H:%M:%S')
+        str_assigned = datetime.utcfromtimestamp(response.timeAssigned).strftime('%Y-%m-%d %H:%M:%S')
+        str_inprogress = datetime.utcfromtimestamp(response.timeInProgress).strftime('%Y-%m-%d %H:%M:%S')
+        str_delivered = datetime.utcfromtimestamp(response.timeDelivered).strftime('%Y-%m-%d %H:%M:%S')
+        str_throughput = str(datetime.utcfromtimestamp(response.timeDelivered)
+                             - datetime.utcfromtimestamp(response.timeUnassigned))
+
         print("Order:{} Vehicle:{} UNASSIGNED:{} ASSIGNED:{} IN_PROGRESS:{} DELIVERED:{} ThroughputTime:{}".format(
-            response.id, response.vehicle,
-            datetime.utcfromtimestamp(response.timeUnassigned).strftime('%Y-%m-%d %H:%M:%S'),
-            datetime.utcfromtimestamp(response.timeAssigned).strftime('%Y-%m-%d %H:%M:%S'),
-            datetime.utcfromtimestamp(response.timeInProgress).strftime('%Y-%m-%d %H:%M:%S'),
-            datetime.utcfromtimestamp(response.timeDelivered).strftime('%Y-%m-%d %H:%M:%S'),
-            str(datetime.utcfromtimestamp(response.timeDelivered) - datetime.utcfromtimestamp(response.timeUnassigned))
-            ),
+            response.id, response.vehicle, str_unassigned, str_assigned, str_inprogress, str_delivered, str_throughput),
             flush=True
         )
+
+        query = "INSERT INTO reports (order, vehicle, unassigned, assigned, inprogress, delivered, throughput)" \
+                "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        db_cursor.execute(query, (response.id, response.vehicle,
+                                  str_unassigned, str_assigned, str_inprogress, str_delivered, str_throughput))
+        db_connection.commit()
 
 
 def handler(number, frame):
---
+++ order-monitoring/dashboard/app.py
@@ -0,0 +1,46 @@
+from sqlalchemy import create_engine
+import pymysql
+import flask
+import dash
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
+app.layout = html.Div(children=[
+    html.H1(children='Overview Completed Tours'),
+
+    html.Div(
+        id="table-placeholder"
+    ),
+
+    dcc.Interval(
+        id='interval-component',
+        interval=10*1000
+    )
+])
+
+
+@app.callback(Output('table-placeholder', 'children'),
+              [Input('interval-component', 'n_intervals')])
+def update_metrics(n):
+    connection_str = 'mysql+pymysql://user:password@database:3306/lieferbot'
+    connection = create_engine(connection_str)
+    df = pandas.read_sql(
+        "select * from reports",
+        con=connection
+    )
+    table = dash_table.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict('records'),
+    )
+    return table
+
+
+if __name__ == '__main__':
+    app.run_server(host='0.0.0.0', port=8050)
